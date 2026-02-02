ca445ae936f60f31103baec6337bdd79e6f5b181
Clyde Watson <watso408@msu.edu>
2026-02-02 14:46:59 -0500

    increment version number

--- TARDIS_Spectrum_Filtering/__init__.py
+++ TARDIS_Spectrum_Filtering/__init__.py
@@ -1,1 +1,7 @@
+"""Top-level package for TARDIS-Spectrum-Filtering."""
+
+__author__ = """Clyde Watson"""
+__email__ = "clyde.n.watson@gmail.com"
+__version__ = "1.0.5"
+
 from .Spectrum_Filtering import (get_url_from_config, download_filter, get_filter, interp_filter, plot_filtered_spectrum, plot_filter)
