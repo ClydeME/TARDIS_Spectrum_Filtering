c0378d341e3b773459e5a8e7603ab058f3a568e6
Clyde Watson <watso408@msu.edu>
2026-02-02 15:49:09 -0500

    Fixed init again

--- TARDIS_Spectrum_Filtering/__init__.py
+++ TARDIS_Spectrum_Filtering/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Clyde Watson"""
 __email__ = "clyde.n.watson@gmail.com"
 __version__ = "1.0.5"
 
-from .Spectrum_Filtering import (get_url_from_config, download_filter, get_filter, interp_filter, plot_filtered_spectrum, plot_filter)
+import TARDIS_Spectrum_Filtering.Spectrum_Filtering
